dea66d701fd117cbbc05636570ce8e2aad3bc26c
Isakov Khojiakbarkhon <72197872+Isakov56@users.noreply.github.com>
2024-11-15 12:46:34 +0100

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedLayoutComponent } from 'shared-layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
