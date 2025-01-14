e6413c40e0c5a21da39e8c806005be79db313748
Isakov Khojiakbarkhon <72197872+Isakov56@users.noreply.github.com>
2025-01-14 12:37:52 +0100

    infrastructre done

---
+++ projects/settings/src/lib/infrastructure/details/details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+
+}
